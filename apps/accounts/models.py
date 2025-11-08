06826f5e881fc4b31885776a966dd2990baf0bab
arisu <phomreebna@gmail.com>
2025-11-09 00:46:51 +0700

    change user id to uuid

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,8 +1,10 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from apps.accounts.managers import UserManager
 
 class User(AbstractBaseUser, PermissionsMixin):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     email = models.EmailField(
         verbose_name="email address",
         max_length=255,
